60d0b80a5221adaf7381e16960410c86bb105f58
Abe70 <73710456+Abe70@users.noreply.github.com>
2020-10-31 15:23:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["link project"]
+const urls = ["https://maroon-lush-vanadium.glitch.met"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
